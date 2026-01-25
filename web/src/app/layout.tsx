75c00e2ce22606c1774e166c335e9ce73c035271
Seohy <seohy@example.com>
2026-01-25 23:07:05 +0900

    fix: change adsense script strategy to better support verification

--- web/src/app/layout.tsx
+++ web/src/app/layout.tsx
@@ -57,7 +57,7 @@ export default function RootLayout({
           async
           src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2947913248390883"
           crossOrigin="anonymous"
-          strategy="afterInteractive"
+          strategy="beforeInteractive"
         />
         {/* Google Analytics */}
         <Script
