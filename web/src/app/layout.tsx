c1c67cb2b662c2042865e0ad88201cf271f13364
Seohy <seohy@example.com>
2026-01-25 22:58:19 +0900

    feat: add adsense script

--- web/src/app/layout.tsx
+++ web/src/app/layout.tsx
@@ -52,6 +52,13 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         {children}
+        {/* Google AdSense */}
+        <Script
+          async
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2947913248390883"
+          crossOrigin="anonymous"
+          strategy="afterInteractive"
+        />
         {/* Google Analytics */}
         <Script
           src="https://www.googletagmanager.com/gtag/js?id=G-WHHQ1PBQRE"
